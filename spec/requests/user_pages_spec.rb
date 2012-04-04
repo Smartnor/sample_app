435a51aa327f48fcd2f7693600f41a8f016a89a8
Andrew T. Flower <andrewtflower@gmail.com>
2012-04-04 14:30:36 -0400

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,4 +18,43 @@ describe "User pages" do
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: 'Sign up') }
   end
+
+  describe "signup" do
+    before { visit signup_path }
+
+    describe "with invalid information" do
+      before do
+        fill_in "Name",         with: "Foo Bar"
+        fill_in "Email",        with: "foo@invalid"
+        fill_in "Password",     with: "dude"
+        fill_in "Confirmation", with: "dude"
+      end
+
+      it "should not create a user" do
+        expect { click_button "Create my account" }.not_to change(User, :count)
+      end
+
+      describe "error messages" do
+        before { click_button "Create my account" }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect do
+          click_button "Create my account"
+        end.to change(User, :count).by(1)
+      end
+    end
+  end
 end
