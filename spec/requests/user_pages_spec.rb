5162aa7621ccb056400f754a8cb10b5b1d18f466
Andrew T. Flower <andrewtflower@gmail.com>
2012-04-09 14:49:02 -0400

    End of Chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,6 +21,7 @@ describe "User pages" do
 
   describe "signup" do
     before { visit signup_path }
+    let(:submit) { "Create my account" }
 
     describe "with invalid information" do
       before do
@@ -31,11 +32,11 @@ describe "User pages" do
       end
 
       it "should not create a user" do
-        expect { click_button "Create my account" }.not_to change(User, :count)
+        expect { click_button submit }.not_to change(User, :count)
       end
 
       describe "error messages" do
-        before { click_button "Create my account" }
+        before { click_button submit }
 
         it { should have_selector('title', text: 'Sign up') }
         it { should have_content('error') }
@@ -52,8 +53,16 @@ describe "User pages" do
 
       it "should create a user" do
         expect do
-          click_button "Create my account"
+          click_button submit
         end.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
