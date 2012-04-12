dc8f7a5c71f3baf9b803244fdb06ef21f614845d
Andrew T. Flower <andrewtflower@gmail.com>
2012-04-11 22:47:39 -0400

    End of Chapter 8

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -13,8 +13,38 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
+def have_full_title(message)
+  have_title(full_title(message))
+end
+
+RSpec::Matchers.define :have_errors do
+  match do |page|
+    page.should have_selector('div.alert.alert-error')
+  end
+end
+
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
   end
 end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_title do |message|
+  match do |page|
+    page.should have_selector('title', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_heading do |message|
+  match do |page|
+    page.should have_selector('h1', text: message)
+  end
+end
+
+
